7e68acfee0ba6516c099f2b363fc99e689a0c4d5
Rohan Arni <rtenacity@gmail.com>
2024-04-21 15:42:23 -0400

    commented

--- foucault.py
+++ foucault.py
@@ -2,25 +2,32 @@ import adi
 import time
 from simple_pid import PID
 
+# Initialize ADC and DAC through the module
 DAC = adi.ad579x(uri="ip:analog.local", device_name='ad5791')
 ADC = adi.ad7124(uri = 'ip:analog.local')
 
+# Define sampling rate and voltage scales
 ADC.sample_rate = 711
 dac_scale = DAC.channel[0].scale
 adc_scale = ADC.channel[0].scale
 
+# Function to clear noise from the ADC based on regression tests
 def clear_noise(ADC) -> float:
     return float(ADC + (ADC * 0.0001539431268630207) + (-0.014096716462759368))
 
+# Write a value (in mV) to the DAC
 def write_to_dac(val) -> None:
     DAC.channel[0].raw = val / dac_scale
     
+# Read the DAC (in mV)
 def read_dac() -> float:
     return float(DAC.channel[0].raw * dac_scale)
 
+# Read the ADC (in mV), with an option to read a different channel if needed
 def read_adc(chn = 0) -> float:
     return clear_noise(float(ADC.channel[chn].raw * adc_scale))
 
+# Calculate the target value
 PHASEDET_SUMVOLT = 0
 for i in range(1, 20):
     PHASEDET_REFVOLT = int(read_adc(1))
@@ -30,6 +37,7 @@ for i in range(1, 20):
     
 print(PHASEDET_AVGVOLT)
 
+# Define PID controllers for course and fine tuning
 coarse_tune = PID(0.6, 0.1, 0.01, setpoint=PHASEDET_AVGVOLT)
 coarse_tune.output_limits = (-4000, 4000)
 fine_tune = PID(0.6, 0.1, 0.05, setpoint=PHASEDET_AVGVOLT)
@@ -37,6 +45,7 @@ fine_tune.output_limits = (-500, 500)
 
 print(time.strftime("%Y-%m-%d %H:%M:%S"))
 
+# Define computational variables
 stable = True
 limit = 100
 voltage = 800
@@ -53,12 +62,17 @@ while True:
     phase_voltage = sum(phase_voltages) / len(phase_voltages)
     phase_array.append(phase_voltage)
     
+    # Keep the phase array at 5 values
+    if len(phase_array) > 5:
+        phase_array.pop(0)
+
     # Calculate the error to determine whether to use coarse correction or fine correction
     error = PHASEDET_AVGVOLT - phase_voltage
     
+    # Check if a signal is stable based on the last five values in the phase array
     stable = True
     if len(phase_array) > 10:
-        last = phase_array[-5:]
+        last = phase_array
         for voltage in last:
             if abs(PHASEDET_AVGVOLT - voltage) > 5:
                 stable = False
@@ -66,13 +80,14 @@ while True:
     else:
         stable = False
     
-    
+    # If the signal isn't stable our outside the limit implement course correction
     if abs(error) > limit or not stable:
         correction_course = voltage + ((coarse_tune(phase_voltage) / 1000) * voltage)
         write_to_dac(correction_course)
         print(f"PID_c: {PHASEDET_AVGVOLT:.3f}, {phase_voltage:.3f}, {correction_course}, {phase_voltage - PHASEDET_AVGVOLT:.3f}, {stable}")
         time.sleep(0.1)
 
+    # If the signal is stable implement fine correction
     else:
         correction_fine = correction_course + ((fine_tune(phase_voltage) / 1000) * voltage)
         write_to_dac(correction_fine)
